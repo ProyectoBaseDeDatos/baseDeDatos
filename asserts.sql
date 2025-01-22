eb9ef6c18ee9d2913b8ddddcd9c1303ebfdafc68
= <luis019dell@gmail.com>
2025-01-22 15:31:36 -0600

    assert

--- asserts.sql
+++ asserts.sql
@@ -8,7 +8,7 @@ DECLARE
     limite INTEGER;
 BEGIN
     SELECT COUNT(*) INTO total_species FROM Especimen WHERE ID_Evento_Recoleccion = NEW.ID_Evento_Recoleccion;
-    SELECT limite INTO limite FROM Evento_de_Coleccion WHERE ID = NEW.ID_Evento_Recoleccion;
+    SELECT maximo_de_especies INTO limite FROM Evento_de_Coleccion WHERE ID = NEW.ID_Evento_Recoleccion;
 
     IF total_species > limite THEN
         RAISE EXCEPTION 'El número de especies recolectadas excede el límite permitido.';
